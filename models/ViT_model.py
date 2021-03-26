268e86f954e728c0b3918e16b60e951f1b8ba9ee
ale-ubuntu <alessandro.arezzo@stud.unifi.it>
2021-03-26 19:08:21 +0100

    Refactor

--- models/ViT_model.py
+++ models/ViT_model.py
@@ -119,25 +119,15 @@ class MultiheadAttention(nn.Module):
             nn.Dropout(dropout)
         )
 
-    def forward(self, x, mask = None):
+    def forward(self, x):
         b, n, _, h = *x.shape, self.n_heads
-        qkv = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
-
-        dots = einsum('bhid,bhjd->bhij', q, k) * self.scale
-
-        if mask is not None:
-            mask = pad(mask.flatten(1), (1, 0), value = True)
-            assert mask.shape[-1] == dots.shape[-1], 'mask has incorrect dimensions'
-            mask = mask[:, None, :] * mask[:, :, None]
-            dots.masked_fill_(~mask, float('-inf'))
-            del mask
-
-        attn = dots.softmax(dim=-1)
-
-        out = einsum('bhij,bhjd->bhid', attn, v)
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        query, keys, values = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
+        dots = einsum('bhid,bhjd->bhij', query, keys) * self.scale
+        scores = dots.softmax(dim=-1)
+        out = einsum('bhij,bhjd->bhid', scores, values)
         out = rearrange(out, 'b h n d -> b n (h d)')
-        out =  self.to_out(out)
+        out = self.to_out(out)
         return out
 
 
